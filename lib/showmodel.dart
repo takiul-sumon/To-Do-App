afd6b107cebbd74902ce06b455f38829736c6556
Md Takiul Islam Sumon <Takiulislam20@gmail.com>
2025-02-01 23:37:47 +0600

    Show Model

--- lib/showmodel.dart
+++ lib/showmodel.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class BottomSheet extends StatefulWidget {
   const BottomSheet({super.key});
 
@@ -9,56 +10,54 @@ class BottomSheet extends StatefulWidget {
 class _BottomSheetState extends State<BottomSheet> {
   @override
   Widget build(BuildContext context) {
-      List<String> priority_List = ['Low', 'Medium', 'High'];
+    List<String> priority_List = ['Low', 'Medium', 'High'];
     String currentValue = priority_List[0];
 
-    
     return SizedBox(
-      
-                        height: 600,
-                        width: double.infinity,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            TextField(
-                              decoration: InputDecoration(
-                                hintText: 'Enter your text here',
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(20.0),
-                                ),
-                              ),
-                            ),
-                            Text('Select Priority'),
-                            Row(
-                              children: [
-                                Radio(
-                                    value: 'Low',
-                                    groupValue: currentValue,
-                                    onChanged: (value) {
-                                      setState(() {
-                                        currentValue = value.toString();
-                                      });
-                                    }),
-                                     Radio(
-                                    value: 'Medium',
-                                    groupValue: currentValue,
-                                    onChanged: (value) {
-                                      setState(() {
-                                        currentValue = value.toString();
-                                      });
-                                    }),
-                                     Radio(
-                                    value: 'High',
-                                    groupValue: currentValue,
-                                    onChanged: (value) {
-                                      setState(() {
-                                        currentValue = value.toString();
-                                      });
-                                    })
-                              ],
-                            )
-                          ],
-                        ),
-                      );
+      height: 600,
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              hintText: 'Enter your text here',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+            ),
+          ),
+        const  Text('Select Priority'),
+          Row(
+            children: [
+              Radio(
+                  value: 'Low',
+                  groupValue: currentValue,
+                  onChanged: (value) {
+                    setState(() {
+                      currentValue = value.toString();
+                    });
+                  }),
+              Radio(
+                  value: 'Medium',
+                  groupValue: currentValue,
+                  onChanged: (value) {
+                    setState(() {
+                      currentValue = value.toString();
+                    });
+                  }),
+              Radio(
+                  value: 'High',
+                  groupValue: currentValue,
+                  onChanged: (value) {
+                    setState(() {
+                      currentValue = value.toString();
+                    });
+                  })
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
