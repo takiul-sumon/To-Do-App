1e9b9e5d091a3d33bf8ba164fa903b44e9124fae
Md Takiul Islam Sumon <Takiulislam20@gmail.com>
2025-02-05 21:56:24 +0600

    Firebase Connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:to_do/My_Home_Page.dart';
+import 'package:to_do/firebase_options.dart';
 import 'package:to_do/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
